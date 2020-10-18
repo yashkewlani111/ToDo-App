9845c62980190ad57d761b8b3d6f10a13f91b02d
yashkewlani <yashkewlani2800@gmail.com>
2020-10-18 19:57:56 +0530

    Updates

---
+++ precache-manifest.b0a221fa548d291fa351123a507505a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "851ad9517b369a190979ecb38ebdadd2",
+    "url": "/ToDo-App/index.html"
+  },
+  {
+    "revision": "8a53efbfe6b3164f9156",
+    "url": "/ToDo-App/static/css/main.031e540d.chunk.css"
+  },
+  {
+    "revision": "2f58a026614d36d51ce7",
+    "url": "/ToDo-App/static/js/2.fedf2b09.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/ToDo-App/static/js/2.fedf2b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a53efbfe6b3164f9156",
+    "url": "/ToDo-App/static/js/main.8a351b9d.chunk.js"
+  },
+  {
+    "revision": "d808f947c4358d228014",
+    "url": "/ToDo-App/static/js/runtime-main.d203c3ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ToDo-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
